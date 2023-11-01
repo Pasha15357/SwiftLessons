9a7213152734e57b65de3587d66ca0a26338894e
Pavel Gavrilov <gavrilovpasha27@gmail.com>
2023-11-01 16:56:16 +0300

    Initial Commit

---
+++ SwiftLessons/SwiftLessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLessonsApp.swift
+//  SwiftLessons
+//
+//  Created by Паша on 1.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
