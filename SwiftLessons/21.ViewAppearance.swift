9bc8d6e918761b91b2c26abadcc8536ea7594cab
Pavel Gavrilov <gavrilovpasha27@gmail.com>
2023-11-16 17:16:38 +0300

    21-22 lessons, animations and appearance

---
+++ SwiftLessons/21.ViewAppearance.swift
@@ -0,0 +1,18 @@
+//
+//  21.ViewAppearance.swift
+//  SwiftLessons
+//
+//  Created by Паша on 13.11.23.
+//
+
+import SwiftUI
+
+struct _1_ViewAppearance: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _1_ViewAppearance()
+}
---
+++ SwiftLessons/22.Animation.swift
@@ -0,0 +1,18 @@
+//
+//  22.Animation.swift
+//  SwiftLessons
+//
+//  Created by Паша on 16.11.23.
+//
+
+import SwiftUI
+
+struct _2_Animation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _2_Animation()
+}
